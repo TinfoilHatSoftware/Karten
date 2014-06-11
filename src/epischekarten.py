08c57a06bea18d7044d4e48b1338a798977e8618
Zenerboson <zenerboson@gmail.com>
2014-06-11 18:19:50 -0500

    First commit. Not completely ready, almost done.

---
+++ src/epischekarten.py
@@ -0,0 +1,410 @@
+#  epischekarten.py
+#  
+#  Copyright 2014 Jacob Swart
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+n = "[epsiche karten]"
+print (n+"Welcome to the Epische Karten map editor!")
+print (""" 
+ _____      _          _          _   __           _                                           
+|  ___|    (_)        | |        | | / /          | |                                          
+| |__ _ __  _ ___  ___| |__   ___| |/ /  __ _ _ __| |_ ___ _ __                                
+|  __| '_ \| / __|/ __| '_ \ / _ \    \ / _` | '__| __/ _ \ '_ \                               
+| |__| |_) | \__ \ (__| | | |  __/ |\  \ (_| | |  | ||  __/ | | |                              
+\____/ .__/|_|___/\___|_| |_|\___\_| \_/\__,_|_|   \__\___|_| |_|                              
+     | |                                                                                       
+     |_|""")
+import time
+print(n+"Module time loaded.")
+import pygame
+print(n+"Module pygame loaded.")
+import PIL.Image,PIL.ImageTk
+print(n+"PIL.Image, PIL.ImageTk imported from module PIL.")
+import io
+print(n+"Module io loaded.")
+import libkarten
+print(n+"Module libkarten loaded.")
+from tkinter import *
+print(n+"* imported from module tkinter.")
+from tkinter import ttk
+print(n+"ttk imported from module tkinter.")
+from tkinter import filedialog
+print(n+"filedialog imported from module tkinter.")
+from tkinter import messagebox
+print(n+"messagebox imported from module tkinter.")
+import os
+print(n+"Module os loaded.")
+import sys
+print(n+"Module sys loaded.")
+print(n+"Defining global variables.")
+map_width = 0
+map_height = 0
+screen_width = 0
+screen_height = 0
+camera_surface = None
+camera_pos = [0,0]
+myclock = None
+screen = None
+fps_limit = 0
+running = True
+reqs_update=[]
+current_tile = None
+global tile_buttons
+tile_buttons=[]
+tilesets={}
+global button_imgs
+button_imgs=[]
+counter=0
+global current_tset
+current_tset=''
+global counter
+global current_tile
+current_tile=None
+global current_layer
+current_layer=None
+global current_layer_str
+current_collision_layers=[]
+print(n+"Defining layers.")
+layer1 = pygame.sprite.Group()
+layer2 = pygame.sprite.Group()
+layer3 = pygame.sprite.Group()
+layer4 = pygame.sprite.Group()
+layer5 = pygame.sprite.Group()
+layer1_c = []
+layer2_c = []
+layer3_c = []
+layer4_c = []
+layer5_c = []
+selected_tiles=[]
+nontemp_selected_tiles=[]
+tiles=[]
+print(n+"Layer definitions completed.")
+print(n+"Defining global functions.")
+testtile=None	
+def round32(x): return (x+0b10000) & (~0b11111)
+def bad_input():
+	print(n+"Invalid input! Quitting!")
+	quit()
+def quitapp():
+	print(n+"Quitting on user request.")
+	running = False
+def load_tileset():
+	global counter
+	global current_tset
+	folderpath = filedialog.askdirectory()
+	print(folderpath)
+	current_tset=os.path.basename(folderpath)
+	try:
+		tilesets[current_tset]=libkarten.XMLTileSet(folderpath)
+	except:
+		print(n+'Invalid tileset directory!')
+		return
+	update_tile_buttons()
+	list_tsets_box.insert(END,os.path.basename(folderpath))
+def change_tset(spam_eggs):
+	time.sleep(0.05)
+	selection = list_tsets_box.curselection()
+	current_tset=list_tsets_box.get(selection[0])
+	update_tile_buttons()
+def update_tile_buttons():
+	global counter
+	global current_tset
+	global button_imgs
+	global tile_buttons
+	for button in tile_buttons:
+		button.destroy()
+	tile_buttons=[]
+	button_imgs=[]
+	for tile in tilesets[current_tset].tiles:
+		pygame_image = tile.animation.states[list(tile.animation.states.keys())[0]][0]
+		pil_string_image = pygame.image.tostring(pygame_image,"RGBA")
+		pil_image = PIL.Image.fromstring("RGBA",(pygame_image.get_rect().width,pygame_image.get_rect().height),pil_string_image)
+		button_imgs.append(PIL.ImageTk.PhotoImage(pil_image))
+		tile_buttons.append(Button(tilesets_frame.interior,image=button_imgs[counter]))
+		tile_buttons[counter].bind("<ButtonRelease-1>",change_tile)
+		tile_buttons[counter].meta_number=counter
+		tile_buttons[counter].pack()
+		counter+=1
+	counter=0
+def change_tile(event):
+	global current_tile
+	current_tile=tilesets[current_tset].tiles[event.widget.meta_number]
+def add_tile():
+	mpos_l=[0,0]
+	mpos_l[0],mpos_l[1]=mse
+	mpos_l[0]-=camera_pos[0]
+	mpos_l[1]-=camera_pos[1]
+	tiles.append(libkarten.Tile(current_tile,current_layer,mpos_l,current_collision_layers))
+	print(n+"Tile of type "+str(current_tile)+" added at position "+str(mpos_l)+" on layer "+str(current_layer)+" on collision layers: "+str(current_collision_layers)+".")
+def update_layer():
+	global current_layer_str
+	global current_layer
+	if str(current_layer_str.get())=='layer1':
+		current_layer=layer1
+	elif str(current_layer_str.get())=='layer2':
+		current_layer=layer2
+	elif str(current_layer_str.get())=='layer3':
+		current_layer=layer3
+	elif str(current_layer_str.get())=='layer4':
+		current_layer=layer4
+	elif str(current_layer_str.get())=='layer5':
+		current_layer=layer5
+	print(n+"Current layer changed to "+str(current_layer)+".")
+def update_collision_layers():
+	print(n+"Updating collision layers to:")
+	current_collision_layers=[]
+	for index,layer_svar in enumerate(COL_LAYER_ENABLED):
+		if str(layer_svar.get())=='1':
+			if index+1==1:
+				current_collision_layers.append(layer1_c)
+			if index+1==2:
+				current_collision_layers.append(layer2_c)
+			if index+1==3:
+				current_collision_layers.append(layer3_c)
+			if index+1==4:
+				current_collision_layers.append(layer4_c)
+			if index+1==5:
+				current_collision_layers.append(layer5_c)
+	print(current_collision_layers)
+		
+		
+try:
+	screen_width = int(640)
+	screen_height = int(480)
+except:
+	bad_input()
+print(n+"Defining classes.")
+class cameraScroller(object):
+	def __init__(self):
+		reqs_update.append(self)
+		print(n+"cameraScroller initialized.")
+	def update(self,delta):
+		keys=pygame.key.get_pressed()
+		if keys[pygame.K_LEFT]:
+			camera_pos[0]+=3
+		if keys[pygame.K_RIGHT]:
+			camera_pos[0]-=3
+		if keys[pygame.K_UP]:
+			camera_pos[1]+=3
+		if keys[pygame.K_DOWN]:
+			camera_pos[1]-=3
+class VerticalScrolledFrame(Frame):
+    """A pure Tkinter scrollable frame that actually works!
+    * Use the 'interior' attribute to place widgets inside the scrollable frame
+    * Construct and pack/place/grid normally
+    * This frame only allows vertical scrolling
+
+    """
+    def __init__(self, parent, *args, **kw):
+        Frame.__init__(self, parent, *args, **kw)            
+
+        # create a canvas object and a vertical scrollbar for scrolling it
+        vscrollbar = Scrollbar(self, orient=VERTICAL)
+        vscrollbar.pack(fill=Y, side=RIGHT, expand=FALSE)
+        canvas = Canvas(self, bd=0, highlightthickness=0,
+                        yscrollcommand=vscrollbar.set)
+        canvas.pack(side=LEFT, fill=BOTH, expand=TRUE)
+        vscrollbar.config(command=canvas.yview)
+
+        # reset the view
+        canvas.xview_moveto(0)
+        canvas.yview_moveto(0)
+
+        # create a frame inside the canvas which will be scrolled with it
+        self.interior = interior = Frame(canvas)
+        interior_id = canvas.create_window(0, 0, window=interior,
+                                           anchor=NW)
+
+        # track changes to the canvas and frame width and sync them,
+        # also updating the scrollbar
+        def _configure_interior(event):
+            # update the scrollbars to match the size of the inner frame
+            size = (interior.winfo_reqwidth(), interior.winfo_reqheight())
+            canvas.config(scrollregion="0 0 %s %s" % size)
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the canvas's width to fit the inner frame
+                canvas.config(width=interior.winfo_reqwidth())
+        interior.bind('<Configure>', _configure_interior)
+
+        def _configure_canvas(event):
+            if interior.winfo_reqwidth() != canvas.winfo_width():
+                # update the inner frame's width to fill the canvas
+                canvas.itemconfigure(interior_id, width=canvas.winfo_width())
+        canvas.bind('<Configure>', _configure_canvas)	
+	
+
+input(n+"Creating windows, please keep the command prompt in view at all times in case of important messages. Press enter to continue.")
+print(n+"Setting up tkinter and pygame SDL environ variables.")
+root = Tk()
+root.wm_title("Epische Karten Map Editor")
+current_layer=layer1
+current_layer_str=StringVar()
+current_layer_str.set("layer1")
+embed = Frame(root, width=screen_width, height=screen_height)
+embed.grid(row=0,column=2)
+text = ttk.Button(root, text='Load Tileset', command=load_tileset)
+text.grid(row=2,column=2)
+is_adding_tiles_v=StringVar()
+is_adding_tiles=ttk.Checkbutton(root,text="Add tiles",variable=is_adding_tiles_v)
+is_adding_tiles.grid(row=1,column=4)
+snap_to_grid_v=IntVar()
+snap_to_grid=ttk.Checkbutton(root,text="Snap to grid",variable=snap_to_grid_v)
+snap_to_grid.grid(row=0,column=4)
+Label(text="Tilesets").grid(row=3,column=0)
+Label(text="Press delete key to delete tiles.").grid(row=1,column=2)
+list_tsets_box=Listbox(root)
+list_tsets_box.grid(row=1,column=0)
+list_tsets_box.bind("<<ListboxSelect>>", change_tset)
+tsets_scrollbar = Scrollbar(root, orient=VERTICAL)
+tsets_scrollbar.config(command=list_tsets_box.yview)
+tsets_scrollbar.grid(row=1,column=1,sticky='ns')
+tilesets_frame = VerticalScrolledFrame(root, width=screen_width, height = 400)
+tilesets_frame.grid(row=0,column=0)
+tilesets_frame.interior.config(width=128)
+MODES = [
+        ("Layer 1", "layer1",1),
+        ("Layer 2", "layer2",2),
+        ("Layer 3", "layer3",3),
+        ("Layer 4", "layer4",4),
+        ("Layer 5", "layer5",5)
+    ]
+for text, mode, row in MODES:
+	b = ttk.Radiobutton(root, text=text,variable=current_layer_str, value=mode, command = update_layer)
+	b.grid(row=row+1,column=3)
+COLLISION_LAYERS = [
+        ("Collision Layer 1", "layer1",1),
+        ("Collision Layer 2", "layer2",2),
+        ("Collision Layer 3", "layer3",3),
+        ("Collision Layer 4", "layer4",4),
+        ("Collision Layer 5", "layer5",5)
+    ]
+COL_LAYER_ENABLED = [
+	StringVar(),
+	StringVar(),
+	StringVar(),
+	StringVar(),
+	StringVar()
+	]
+for text, mode, row in COLLISION_LAYERS:
+	b = ttk.Checkbutton(root, text=text,variable=COL_LAYER_ENABLED[row-1], command = update_collision_layers)
+	b.grid(row=row+1,column=5)
+root.update()
+os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+os.environ['SDL_VIDEODRIVER'] = 'windib'
+print(n+"Done.")
+print(n+"Initializing pygame.time.Clock object.")
+myclock = pygame.time.Clock()
+print(n+"Initial tick of clock occurs now.")
+try:
+	fps_limit = 60
+except:
+	bad_input()
+myclock.tick(fps_limit)
+responsetext = input(n+"Create map or open existing? [reply C for create or O for open.]>>>")
+if responsetext.lower()=="o":
+	pass
+elif responsetext.lower()=="c":
+	try:
+		map_width = int(input(n+"Width of map? [in pixels]>>>"))
+		map_height = int(input(n+"Height of map? [in pixels]>>>"))
+	except:
+		bad_input()
+	print(n+"Creating camera surface.")
+	try:
+		camera_surface = pygame.surface.Surface((map_width,map_height))
+	except:
+		print("Unexpected fatal error while creating camera surface:", sys.exc_info()[0])
+		quit()
+	print(n+"Setting up graphics frame.")
+	pygame.display.init()
+	screen = pygame.display.set_mode((screen_width,screen_height))
+	pygame.display.flip()
+	print(n+"Initializing camera scroller.")
+	scroller = cameraScroller()
+	
+	
+		
+else:
+	bad_input()
+print(n+"Setup completed!")
+while running:
+	delta = myclock.tick(60)
+	try:
+		adding_tiles=int(is_adding_tiles_v.get())
+	except: adding_tiles=0
+	selected_tiles=[]
+	if adding_tiles!=1:
+		for tiley in tiles:
+			recty=tiley.rect
+			mpos_l=[0,0]
+			mpos_l[0],mpos_l[1]=mse
+			mpos_l[0]-=camera_pos[0]
+			mpos_l[1]-=camera_pos[1]
+			mpos2=(mpos_l[0],mpos_l[1])
+			if recty.collidepoint(mpos2):
+				selected_tiles.append(tiley)
+	mse = pygame.mouse.get_pos()
+	if snap_to_grid_v.get()==1 and adding_tiles==1:
+		mpos_l=[0,0]
+		mpos_l[0],mpos_l[1]=mse
+		mpos_l[0]-=camera_pos[0]
+		mpos_l[1]-=camera_pos[1]
+		mse3=(round32(mpos_l[0]),round32(mpos_l[1]))
+		mse = (mse3[0]+camera_pos[0],mse3[1]+camera_pos[1])
+	
+	for event in pygame.event.get():
+		if event.type == pygame.MOUSEBUTTONDOWN and adding_tiles==1:
+			if current_tile != None:
+				add_tile()
+			else:
+				messagebox.showerror("Epische Karten","Please load a tileset before adding tiles.")
+		if adding_tiles!=1 and event.type == pygame.MOUSEBUTTONDOWN:
+			if selected_tiles!=[]:
+				if selected_tiles[0] in nontemp_selected_tiles:
+					nontemp_selected_tiles.remove(selected_tiles[0])
+				else:
+					nontemp_selected_tiles.append(selected_tiles[0])
+		if event.type==pygame.KEYDOWN:
+			if event.key==pygame.K_DELETE:
+				for ntemptile in nontemp_selected_tiles:
+					tiles.remove(ntemptile)
+					ntemptile.tile_kill()
+				nontemp_selected_tiles=[]
+
+	for actor in reqs_update:
+		actor.update(delta)
+	camera_surface.fill((128,128,128))
+	screen.fill((0,0,0))
+	layer1.draw(camera_surface)
+	layer2.draw(camera_surface)
+	layer3.draw(camera_surface)
+	layer4.draw(camera_surface)
+	layer5.draw(camera_surface)
+	if adding_tiles==1:
+		nontemp_selected_tiles=[]
+	for tilentemp in nontemp_selected_tiles:
+		pygame.draw.rect(camera_surface,(0,255,0,30),tilentemp.rect,2)
+	for tiles_s_temp in selected_tiles:
+		pygame.draw.rect(camera_surface,(255,0,0,30),tiles_s_temp.rect,1)
+	screen.blit(camera_surface,(camera_pos[0],camera_pos[1]))
+	if adding_tiles==1:
+		try:
+			ctile_rect=current_tile.image.get_rect()
+			ctile_rect.x,ctile_rect.y=mse
+			screen.blit(current_tile.image,ctile_rect)
+		except AttributeError: pass
+	pygame.display.flip()
+	try:
+		root.update()
+	except TclError:
+		input(n+"Quit button pressed. Press enter to exit.")
+		sys.exit()
+
+	
+	
+
+
+
+
---
+++ src/libkarten.py
@@ -0,0 +1,138 @@
+#  libkarten.py
+#  
+#  Copyright 2014 Jacob Swart
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+import animations
+import pygame
+import os
+import xml.etree.cElementTree as ET
+
+class TileTemplate(object):
+	def __init__(self,animation,play_anim=None,start_state="off"):
+		self.animation=animation
+		self.image=self.animation.states[list(self.animation.states.keys())[0]][0]
+		self.rect=self.image.get_rect()
+		self.frame=0
+		self.state=start_state
+class XMLTileSet(object):
+	def __init__(self,folder_name):
+		print("[libkarten]Initializing XMLTileSet class for "+str(folder_name)+".")
+		self.tiles=[]
+		self.path_from_here = os.path.join(folder_name)
+		print("[libkarten]Initializing cElementTree for reading of XML animation data.")
+		self.xml_tree = ET.parse(os.path.join(self.path_from_here,"tileset.xml"))
+		self.xml_tree_root = self.xml_tree.getroot()
+		for tile in self.xml_tree_root.findall("tile"):
+			print("[libkarten]Reading tile data from XML:"+str(tile)+".")
+			if int(tile.get("play"))==0:
+				playanim=False
+			else:
+				playanim=True
+			self.tiles.append(TileTemplate(animations.XMLTileAnimation(folder_name,tile.get("anim")),play_anim=playanim,start_state="off"))
+		self.xml_tree=None
+		self.xml_tree_root=None
+		print(self.tiles)
+class Tile(pygame.sprite.Sprite):
+	def __init__(self,tiletemplate,layer,pos,collision_layers=[]):
+		super(Tile,self).__init__()
+		self.add(layer)
+		self.c_layers=collision_layers
+		self.animation=tiletemplate.animation
+		self.image=tiletemplate.image
+		self.rect=self.image.get_rect()
+		self.frame=0
+		for c_layer in self.c_layers:
+			c_layer.append(self)
+		self.state=tiletemplate.state
+		self.playing=tiletemplate.playing
+		self.rect.x=pos[0]
+		self.rect.y=pos[1]
+	def update_tile(self,delta):
+		self.image=self.animation.get_frame(self.state,self.frame)
+		self.rect=self.image.get_rect
+		if self.playing:
+			self.frame+=1
+		if self.frame==len(self.animation.states[self.state])-1:
+			self.frame=0
+	def play(self):
+		self.playing=True
+	def stop(self):
+		self.playing=False
+		self.frame=0
+	def pause(self):
+		self.playing=False
+	def set_state(self,state):
+		self.state=state
+	def tile_kill(self):
+		for c_layer in self.c_layers:
+			c_layer.remove(self)
+		self.kill()
+		del self
+	def get_collide_layers(self):
+		return self.c_layers
+class Karte(object):
+	def __init__(self):
+		self.tiles=[]
+		self.tilesets={}
+		self.name=""
+	def fromxml(self,map_name):
+		tmptiles=[]
+		self.name=map_name
+		print("[libkarten]Loading Karte xml mapfile from map directory with name "+str(path)+".")
+		path_to_mapfile=os.path.join("..","media","maps_xml",path,".xml")
+		xml_tree = ET.parse(path_to_mapfile)
+		xml_tree_root = xml_tree.getroot()
+		tileset_defs_tag=root.findall("tileset_definitions")[0]
+		for adding_tileset in tileset_defs_tag.findall("tileset"):
+			self.tilesets[adding_tileset.get("name")]=XMLTileSet(adding_tileset.get("name"))
+		tilestag=root.findall("map_tiles")[0]
+		for tiles in tilestag.findall("tile")
+			c_layers=[]
+			layer_temp=layers_l[int(tiles.get("layer")[6])]
+			for col_layer in tiles.get("collision_layers").split()
+				c_layers_temp.append(collisions_l[int(col_layer[6])])
+			tmptiles.append(Tile(self.tilesets[tiles.get("tileset")][int(tiles.get("index"))],layer_temp,(tiles.get(pos).split()[0],tiles.get(pos).split()[1]),c_layers_temp))
+	def update(self,delta):
+		for tile in self.tiles:
+			tile.update(delta)
+	def kill_tile(self,tileref):
+		tileref.tile_kill()
+		self.tiles.remove(tileref)		
+	def add_tile(self,tile_template,layer,pos,collision_layers=[]):
+		tile_addition=Tile(tile_template,layer,pos,collision_layers)
+		self.tiles.append(tile_addition)
+	def get_tile_template(self,tileset_name,index):
+		return tilesets[tileset_name].tiles[index]
+	def uninitialize(self,reference):
+		for tile in self.tiles:
+			tile.tile_kill()
+			del tile
+		self.tiles=None
+		self.tilesets=None
+		self.name=None
+		reference=None
+		self=None
+	def get_tiles_at_pos(self,pos):
+		tmp=[]
+		for tile in self.tiles:
+			if tile.rect.collidepoint(pos):
+				tmp.append(tile)
+		return tmp
+	def load_tileset(self,tset_name):
+		self.tilesets[tset_name]=XMLTileSet(tset_name)
+	def remove_tileset(self,tset_name)
+		#this will not delete current tiles from that tileset
+		self.tilesets[tset_name]=None
+				
+				
+			
+		
+	
+
+		
+		
+	
+
